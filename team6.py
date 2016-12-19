0a9dbb6fe155bb6f593e9d3337dc4a8d86701ffd
Lucas "Rotten" Rotun <lucas.rotun@vtsd.com>
2016-12-19 12:59:15 -0500

    Team6 LucasR and JoeG

--- team6.py
+++ team6.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Latrine' # Only 10 chars displayed.
+strategy_name = 'If we do not know what we are doing, they certainly cannot anticipate our move.'
+strategy_description = 'It is random picking between 1 or 2.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +18,11 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    choice = random.randint(1,2)
+    if choice == 1:
+        return 'c'
+    if choice == 2:
+        return 'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,9 +30,6 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
