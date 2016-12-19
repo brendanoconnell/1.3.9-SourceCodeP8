a41e709510b1117bc25cc60cd2b54971f62c6891
dftmckeon <dftmckeon@gmail.com>
2016-12-19 13:00:46 -0500

    commit

--- team8.py
+++ team8.py
@@ -6,10 +6,10 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
+#Testing
 team_name = 'NotSure TBH' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+strategy_name = 'Ravid Dusnak'
+strategy_description = 'Use of a slight favor of colluding and a strategy analysis.'
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -26,7 +26,50 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    answer = 'c'
+    colludeTotal = 0
+    betrayTotal = 0
+    theirTotal = int(len(their_history))
+    strategy = 0
+    if my_history == '':
+        answer = 'c'
+    if theirTotal <= 50 and theirTotal > 0:
+        if their_history[-1] == 'c':
+            colludeTotal += 1
+        if their_history[-1] == 'b':
+            betrayTotal += 1
+        if their_history[-1] == 'b' and my_history[-1] == 'c':
+            answer = 'b'
+        if their_history[-1] == 'b' and my_history[-1] == 'b':
+            answer = 'b'
+        if their_history[-1] == 'c' and my_history[-1] == 'c':
+            answer = 'c'
+        if their_history[-1] == 'c' and my_history[-1] == 'b':
+            answer = 'c'
+    if theirTotal == 51:
+        if colludeTotal == betrayTotal:
+            strategy = 0
+        if colludeTotal < betrayTotal:
+            strategy = 0
+        if colludeTotal > betrayTotal:
+            strategy = 1
+        answer = 'c'
+    if theirTotal > 51:
+        if strategy == 1:
+            if my_history[-1] == 'c':
+                answer =  'b'
+            elif my_history[-2] == 'c' and my_history[-1] == 'b':
+                answer = 'b'
+            else:
+                answer = 'c'
+        if strategy == 0:
+            if my_history[-1] == 'c':
+                answer =  'c'
+            elif my_history[-2] == 'c' and my_history[-1] == 'c':
+                answer = 'b'
+            else:
+                answer = 'c'  
+    return answer
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
